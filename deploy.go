7e7ae96ac04ad191b79a51e5bc8917c28a988718
praniil <pparajuli15@gmail.com>
2024-07-03 22:20:16 +0545

    feat: created a new client for interacting with ganache. used the account in ganache to get its balance and pending balance information

--- deploy.go
+++ deploy.go
@@ -1,21 +1,52 @@
 package main
 
 import (
+	// "context"
+	// "crypto/ecdsa"
+	// "fmt"
+	// "math/big"
+	// "os"
+
+	// "simple_storage/api"
+	// "github.com/ethereum/go-ethereum/accounts/abi/bind"
+	// "github.com/ethereum/go-ethereum/common"
+	// "github.com/ethereum/go-ethereum/crypto"
 	"context"
 	"fmt"
-	"os"
+	"log"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-	client, err := ethclient.Dial(os.Getenv("RPC_URL"))
+	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	chainId, err := client.NetworkID(context.Background())
-	if err!= nil {
-		panic(err)
+
+	defer client.Close()
+
+	fmt.Println("we have a connection")
+
+	blockNumber := big.NewInt(0)
+
+	account := common.HexToAddress("0x57F013799D0762224852a016C2221F437482CDB5")
+	balance, err := client.BalanceAt(context.Background(), account, blockNumber)
+
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println(chainId);
+
+	//pending balance:= for checking after submitting or waiting for a transaction to be confirmed
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+
+	if err != nil {
+		fmt.Println("failed to display the pending balance due to: ", err)
+	}
+	fmt.Println(pendingBalance)
+	fmt.Println(balance)
+
+
 }
